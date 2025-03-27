e36817c8eb495473d3aaa64d2f2241d438b4ec15
sedg <hyde.rou@yandex.ru>
2025-03-27 23:49:20 +0300

    learn js types

---
+++ head12.js
@@ -0,0 +1,17 @@
+// массив
+let names = ["Jerry", "Elaine", "George", "Kramer"];
+let alsoNames = new Array("Dennis", "Frank", "Dee", "Mac");
+// округленное число
+let roundNumber = Math.round("3.14");
+// текущая дата
+let today = new Date();
+// объект boolean
+let booleanObject = new Boolean(true);
+// бесконечность
+let unquantifiablyBigNumber = Number.POSITIVE_INFINITY;
+// объект string
+let hello = new String("Hello!");
+let movie = "Pulp Fiction";
+let movieObj = new String("Pulp Fiction");
+console.log(movie);
+console.log(movieObj);
